7db8a16b77543577b0461a83d53288c8d9db4c57
Václav Machovský <crocoo.main@gmail.com>
2023-09-17 01:45:25 +0200

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Nope!");
+            return a / b;
+        }
+
+    }
+}
